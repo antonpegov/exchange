bc6f5c2d6623190778c461c4057f12ecd61f42c3
Anton Pegov <antonpegov@gmail.com>
2021-05-20 22:33:14 +0200

    Downgrade RXJS version from 7 to 6

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { SnackbarProvider } from 'notistack';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { SnackbarProvider } from 'notistack'
+import { ConnectedRouter } from 'connected-react-router'
 
-import { store } from 'store';
-import App from 'App';
-import 'index.css';
+import { store, history } from 'store'
+import App from 'App'
+import 'index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <SnackbarProvider maxSnack={5} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
       <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
       </Provider>
     </SnackbarProvider>
   </React.StrictMode>,
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
-import { combineEpics, createEpicMiddleware } from "redux-observable";
-import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
+import { connectRouter } from 'connected-react-router'
+import { createBrowserHistory} from 'history'
+import { combineEpics, createEpicMiddleware } from 'redux-observable'
+import { combineReducers, compose, createStore, applyMiddleware } from 'redux'
 
 import { exchangeReducer } from 'state/reducers/exchange.reducer'
 import { walletReducer } from 'state/reducers/wallet.reducer'
-import { exchangeEpics } from 'state/epics/exchange.epics';
-import { walletEpics } from 'state/epics/wallet.epics';
+import { exchangeEpics } from 'state/epics/exchange.epics'
+import { walletEpics } from 'state/epics/wallet.epics'
 
 declare global {
   interface Window {
@@ -14,23 +16,26 @@ declare global {
 
 const epicMiddleware = createEpicMiddleware()
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const history = createBrowserHistory();
 
 export const rootEpic = combineEpics(
   exchangeEpics,
   walletEpics,
 )
 
-export const rootReducer = combineReducers({
+export const rootReducer = (history: any) => combineReducers({
+  router: connectRouter(history),
   walletReducer,
   exchangeReducer,
-});
+})
 
 export const store = createStore(
-  rootReducer,
+  rootReducer(history),
   composeEnhancers(
    applyMiddleware(epicMiddleware)
   ),
-);
+)
 
 epicMiddleware.run(rootEpic)
